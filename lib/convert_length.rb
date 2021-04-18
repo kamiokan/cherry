9d253cb6ae43efe30345bdea1c5f357d33ed179e
kamiokan <spherenov@gmail.com>
2021-04-18 17:46:41 +0900

    5.5.3 convert_length メソッドを改善する まで

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,4 +1,5 @@
-def convert_length(length, unit_form, unit_to)
-  units = { 'm' => 1.0, 'ft' => 3.28, 'in' => 39.37 }
-  (length / units[unit_form] * units[unit_to]).round(2)
+UNITS = { :m => 1.0, :ft => 3.28, :in => 39.37 }
+
+def convert_length(length, from: unit_form, to: unit_to)
+  (length / UNITS[from] * UNITS[to]).round(2)
 end
