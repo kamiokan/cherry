af210087237dff3821acca3eb445e20bf9f9b1a4
kamiokan <spherenov@gmail.com>
2021-04-22 17:02:29 +0900

    p.232 RDocでAPIドキュメントを作成する まで

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -1,10 +1,17 @@
+# 切符を表すクラス
 class Ticket
   attr_reader :fare, :stamped_at
 
+  # Gateオブジェクトの作成
+  # ==== 引数
+  # * +fare+ - 運賃
   def initialize(fare)
     @fare = fare
   end
 
+  # 乗車駅名を記録する
+  # ==== 引数
+  # * +fare+ - 運賃
   def stamp(name)
     @stamped_at = name
   end
