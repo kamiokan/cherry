c9e9907c23f4e0d3fa8f385e11920e41f5bcf691
kamiokan <spherenov@gmail.com>
2021-04-30 17:19:24 +0900

    ブロックを引数として明示的に受け取る

---
+++ sandbox/10.2.2.rb
@@ -0,0 +1,25 @@
+# 日本語版
+def greeting_ja(&block)
+  texts = ['おはよう', 'こんにちは', 'こんばんは']
+  greeting_common(texts, &block)
+end
+
+# 英語版
+def greeting_en(&block)
+  texts = ['good morning', 'hello', 'good evening']
+  greeting_common(texts, &block)
+end
+
+def greeting_common(texts, &block)
+  puts texts[0]
+  puts block.call(texts[1])
+  puts texts[2]
+end
+
+greeting_ja do |text|
+  text * 2
+end
+
+greeting_en do |text|
+  text * 2
+end
