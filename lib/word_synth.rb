8d6461d7ec8a9f4e84cbfb7275dc2849498dbc35
kamiokan <spherenov@gmail.com>
2021-05-04 18:27:28 +0900

    12.7.2 Rakeを使ったテストの一括実行 まで

--- lib/word_synth.rb
+++ lib/word_synth.rb
@@ -7,7 +7,7 @@ class WordSynth
     @effects << effect
   end
 
-  def play (original_words)
+  def play(original_words)
     @effects.inject(original_words) do |words, effect|
       effect.call(words)
     end
